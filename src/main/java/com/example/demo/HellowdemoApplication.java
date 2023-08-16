e33e66db0ac9babc18ce79532743403e759c09e5
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-08-17 00:35:09 +0200

    updates

---
+++ src/main/java/com/example/demo/HellowdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowdemoApplication.class, args);
+    }
+}
+
